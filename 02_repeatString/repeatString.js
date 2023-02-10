e87a48907e386babd2cff93df0041e58295592a6
chengjie <chengzhou98@gmail.com>
2023-02-10 16:52:05 +0100

    Finished repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeatTimes) {
+  let finalString = "";
+  if (repeatTimes < 0) {
+    return "ERROR";
+  }
+  for (let index = 0; index < repeatTimes; index++) {
+    finalString += string;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
