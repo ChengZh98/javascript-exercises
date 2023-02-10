df598f33c4486fddc35ad9099ce792ad71764d9d
chengjie <chengzhou98@gmail.com>
2023-02-10 18:11:19 +0100

    finised remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+/**
+ *
+ * @param {array} array
+ * @param  {...any} args
+ * @returns
+ */
 
+const removeFromArray = function (array, ...args) {
+  return array.filter((val) => {
+    return args.findIndex((itemToRemove) => itemToRemove === val) === -1;
+  });
 };
 
 // Do not edit below this line
