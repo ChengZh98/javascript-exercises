5428b0f3b49498d121f6ff91a8f6f858d02f8433
chengjie <chengzhou98@gmail.com>
2023-02-14 21:59:46 +0100

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  const parsedNumber = parseInt(n);
 
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (parsedNumber === 0) {
+    return 0;
+  }
+  if (parsedNumber === 1) {
+    return 1;
+  }
+  return fibonacci(parsedNumber - 1) + fibonacci(parsedNumber - 2);
 };
 
 // Do not edit below this line
